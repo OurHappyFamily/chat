3767980e4aede3f2d1933213a6a518988b8c9a97
sdfghjik <1968850831@qq.com>
2017-03-23 16:40:50 +0800

    new pro

--- app/src/main/java/com/example/chat_program/MainActivity.java
+++ app/src/main/java/com/example/chat_program/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.chat_program;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -9,7 +11,17 @@ import android.widget.TextView;
 import com.example.chat_program.act.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textView;
+    private TextView textView, tv1;
+    private static final String SLEEP_TIME_KEY = "time";
+    private static final long SLEEP_TIME = 1000;
+
+    Handler handler = new Handler() {
+//        接受并处理接收到的消息
+        public void handleMessage(Message msg) {
+            String a = msg.getData().getString(SLEEP_TIME_KEY);
+            tv1.setText(a + "s");
+        }
+    };
 
 
     @Override
@@ -18,6 +30,7 @@ private TextView textView;
         setContentView(R.layout.activity_main);
         dianjitiaozhuan();
         shuimian();
+        djs();
     }
 
     private void dianjitiaozhuan() {
@@ -32,6 +45,38 @@ private TextView textView;
         });
     }
 
+    private void djs() {
+        tv1 = (TextView) findViewById(R.id.text_djs1);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int x = 3; x > -1; x--) {
+                    String a = String.valueOf(x);
+//编辑需要发送的消息
+                    Message msg = new Message();
+                    Bundle data = new Bundle();
+                    data.putString(SLEEP_TIME_KEY, a);
+                    msg.setData(data);
+//给handler发送消息
+                    handler.sendMessage(msg);
+
+                    try {
+                        Thread.sleep(SLEEP_TIME);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+
+        }).start();
+
+
+    }
+
     private void shuimian() {
         new Thread(new Runnable() {
             @Override
@@ -41,9 +86,9 @@ private TextView textView;
                     init();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
-                finally {
-                    init();
+                } finally {
+//                    init();
+
                 }
             }
 
@@ -52,10 +97,10 @@ private TextView textView;
     }
 
 
-        private void init() {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-        }
+    private void init() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 
 //        EMClient.getInstance().login("cy","123456", new EMCallBack() {//回调
 //            @Override
@@ -75,7 +120,6 @@ private TextView textView;
 //                Log.d("main", "登录聊天服务器失败！");
 //            }
 //        });
-
 
 
 }
--- app/src/main/java/com/example/chat_program/act/LoginActivity.java
+++ app/src/main/java/com/example/chat_program/act/LoginActivity.java
@@ -10,15 +10,15 @@ import android.widget.Toast;
 
 import com.example.chat_program.MainActivity;
 import com.example.chat_program.R;
-import com.example.chat_program.RegisterActivity;
 
 /**
  * Created by 张泽雅文 on 2017/3/23.
  */
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText edit_username,edit_password;
-    private Button button_login,button_zhuce;
+    private EditText edit_username, edit_password;
+    private Button button_login, button_zhuce;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +27,10 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void init() {
-        edit_username= (EditText) findViewById(R.id.edit_username);
-        edit_password= (EditText) findViewById(R.id.edit_password);
-        button_login= (Button) findViewById(R.id.button_login);
-        button_zhuce= (Button) findViewById(R.id.button_zhuce);
+        edit_username = (EditText) findViewById(R.id.edit_username);
+        edit_password = (EditText) findViewById(R.id.edit_password);
+        button_login = (Button) findViewById(R.id.button_login);
+        button_zhuce = (Button) findViewById(R.id.button_zhuce);
         button_zhuce.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,12 +41,12 @@ public class LoginActivity extends AppCompatActivity {
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username=edit_username.getText().toString();
-                String password=edit_password.getText().toString();
-                if(username.equals("147")&&password.equals("123")){
-                    Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(LoginActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+                String username = edit_username.getText().toString();
+                String password = edit_password.getText().toString();
+                if (username.equals("147") && password.equals("123")) {
+                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
                 }
             }
         });
