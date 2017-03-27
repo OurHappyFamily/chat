13187005f4d90a7899fa6806661ce329e3e77767
sdfghjik <1968850831@qq.com>
2017-03-27 15:29:15 +0800

    new pro

--- app/src/main/java/com/example/chat_program/fragment/SettingFragment.java
+++ app/src/main/java/com/example/chat_program/fragment/SettingFragment.java
@@ -1,11 +1,16 @@
 package com.example.chat_program.fragment;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.chat_program.R;
 
@@ -14,7 +19,9 @@ import com.example.chat_program.R;
  */
 
 public class SettingFragment extends Fragment {
+    private TextView fragment_set_set1, fragment_set_set2, fragment_set_set3, fragment_set_set4, fragment_set_set5;
     private View view;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -23,8 +30,102 @@ public class SettingFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         super.onCreateView(inflater, container, savedInstanceState);
-        view=inflater.inflate(R.layout.fragment_setting,container,false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_setting, container, false);
+        dianji(view);
         return view;
+    }
+
+    /**
+     * 设置各个选项的点击事件
+     *
+     * @param v
+     */
+    public void dianji(View v) {
+        //初始化TextView控件
+        fragment_set_set1 = (TextView) view.findViewById(R.id.fragment_set_set1);
+        fragment_set_set2 = (TextView) view.findViewById(R.id.fragment_set_set2);
+        fragment_set_set3 = (TextView) view.findViewById(R.id.fragment_set_set3);
+        fragment_set_set4 = (TextView) view.findViewById(R.id.fragment_set_set4);
+        fragment_set_set5 = (TextView) view.findViewById(R.id.fragment_set_set5);
+        //给控件添加点击事件
+        fragment_set_set1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //点击弹出提示框
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("设置信息")
+                        .setView(R.layout.alerdialog)//提示框所要显示的布局文件
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("保存", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            }
+        });
+        fragment_set_set2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("消息通知")
+                        .setView(R.layout.alerdialog1)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            }
+        });
+        fragment_set_set3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("聊天")
+                        .setView(R.layout.alerdialog2)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            }
+        });
+        fragment_set_set4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("隐私")
+                        .setView(R.layout.alerdialog3)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            }
+        });
+        fragment_set_set5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("通用")
+                        .setView(R.layout.alerdialog4)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            }
+        });
+
+
     }
 }
