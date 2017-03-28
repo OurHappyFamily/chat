c23d2c8db1575406fccb61783bed6aaa99fdc56c
296130109 <296130109@qq.com>
2017-03-28 08:26:58 +0800

    new pro

--- app/src/main/java/com/example/chat_program/fragment/SettingFragment.java
+++ app/src/main/java/com/example/chat_program/fragment/SettingFragment.java
@@ -9,7 +9,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+
 import android.widget.TextView;
 
 import com.example.chat_program.R;
